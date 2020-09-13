56e5cbae0801c84cd76aee0ec5d66b7a17632378
Washington Silva <sp.washington@gmail.com>
2020-09-13 02:54:30 -0300

    1. Added cucumber feature 2. Change apk to test

---
+++ mobile/features/specs/new_product.feature
@@ -0,0 +1,5 @@
+Feature: Create a new product
+    In app should be possible create a new product in list
+        - Must have a button NEW
+        - Must have a screen to add information about product
+        - Description, amount and unit is a required field
--- mobile/features/support/hooks.rb
+++ mobile/features/support/hooks.rb
@@ -5,7 +5,7 @@ require 'report_builder'
 #     config.input_path = 'log/report_result.json'
 #     config.report_path = 'log/report_result'
 #     config.report_title = "Result of test in APK"
-#     config.additional_info = {System: ENV['PLATFORM'], Perfil: ENV['CAPS']}
+#     config.additional_info = {Device: ENV['DEVICE'], System: ENV['PLATFORM'], Version: ENV['VERSION'], Perfil: ENV['CAPS']}
 #     # config.color = 'black'
 #   end
 #   ReportBuilder.build_report
