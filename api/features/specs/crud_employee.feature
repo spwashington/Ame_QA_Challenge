d77eb71900ff60f628a6271a2a9e89c15d8f8ce6
Washington Silva <sp.washington@gmail.com>
2020-09-13 00:23:02 -0300

    1. Added steps delete a employee

--- api/features/specs/crud_employee.feature
+++ api/features/specs/crud_employee.feature
@@ -1,8 +1,25 @@
 Feature: CRUD API
     API should able to execute a crud in database
+    - Create a new employee
+    - Read a employee
+    - Update a new employee
+    - Delete a employee
 
     Background: Setup API
         Given the API address
+
+    @Crud
+    Scenario: Complete Crud in API
+        Given a data of a new employee
+        |   Name    |   test45    |
+        |   Age     |   35      |
+        |   Salary  |   4500    |
+        When send a post request to create 
+        Then API should return a "success" with message: "Successfully! Record has been added." and Status code: 200
+        And employee should avaliable in database
+        Given employee ID should be delete employee from dabatase
+        Then API should return a "success" with message: "Successfully! Record has been deleted" and status code: 200
+        And employee should not avaliable in database
 
     @CreateEmployee
     Scenario: Create employee
@@ -12,11 +29,10 @@ Feature: CRUD API
         |   Salary  |   4500    |
         When send a post request to create 
         Then API should return a "success" with message: "Successfully! Record has been added." and Status code: 200
-        And employee should avaliable in database
 
     @GetEmployee
     Scenario: Read employee
-        When send a get request with employee 1
+        When send a get request with employee id: 1
         Then Api should return a "success" and status code: 200
 
     ##TODO
@@ -24,4 +40,5 @@ Feature: CRUD API
 
     @DeleteEmployee
     Scenario: Delete employee
-    #remover: t, te, tes, test, test1, test2, test3. test4, test5, test6, test7, test8
+        When send a delete request with employee id: 24
+        Then API should return a "success" with message: "successfully! deleted Records" and status code: 200
--- api/features/step_definitions/api_crud_steps.rb
+++ api/features/step_definitions/api_crud_steps.rb
@@ -9,7 +9,9 @@ end
 
 When("send a post request to create") do
     createUrl = @m_ApiUrl + 'api/v1/create'
-    @m_Response = HTTParty.post(createUrl, :body => {"name":"#{@m_Employee[:Name]}","salary":"#{@m_Employee[:Salary]}","age":"#{@m_Employee[:Age]}"})
+    @m_Response = HTTParty.post(createUrl, :body => {:name => @m_Employee[:Name], :salary => @m_Employee[:Salary], :age => @m_Employee[:Age]})
+    puts @m_Response
+    # @m_Response = HTTParty.post(createUrl, :body => {"name":"#{@m_Employee[:Name]}","salary":"#{@m_Employee[:Salary]}","age":"#{@m_Employee[:Age]}"})
 end
 
 Then("API should return a {string} with message: {string} and Status code: {int}") do |_Status, _Message, _Code|
@@ -18,6 +20,7 @@ Then("API should return a {string} with message: {string} and Status code: {int}
     expect(@m_Response.code).to eql(_Code)
 end
 
+##########COMPLETE CRUD
 Then("employee should avaliable in database") do
     getUrl = @m_ApiUrl + "api/v1/employee/#{@m_Response.parsed_response["data"]["id"]}"
     response = HTTParty.get(getUrl)
@@ -25,8 +28,26 @@ Then("employee should avaliable in database") do
     expect(response.parsed_response["status"]).to eql("success")
 end
 
+Given("employee ID should be delete employee from dabatase") do
+    deleteUrl = @m_ApiUrl + "api/v1/delete/#{@m_Response.parsed_response["data"]["id"]}"
+    response = HTTParty.delete(deleteUrl)
+    expect(response.code).to eql(200)
+    expect(response.parsed_response["status"]).to eql("success")
+end
+
+Then("employee should not avaliable in database") do
+    getUrl = @m_ApiUrl + "api/v1/employee/#{@m_Response.parsed_response["data"]["id"]}"
+    response = HTTParty.get(getUrl)
+    
+    puts ">>>>>>>>>>>>>> CHECAR USER QDO FEZ O DELETE"
+    puts response
+    expect(response.code).to eql(200)
+    expect(response.parsed_response["status"]).to eql("success")
+    puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
+end
+
 ##########GET A EMPLOYEE
-When("send a get request with employee {int}") do |_ID|
+When("send a get request with employee id: {int}") do |_ID|
     getUrl = @m_ApiUrl + "api/v1/employee/#{_ID}"
     @m_Response = HTTParty.get(getUrl)
 end
@@ -34,4 +55,16 @@ end
 Then("Api should return a {string} and status code: {int}") do |_Message, _Code|
     expect(@m_Response.code).to eql(_Code)
     expect(@m_Response.parsed_response["status"]).to eql(_Message)
+end
+
+##########DELETE A EMPLOYEE
+When("send a delete request with employee id: {int}") do |_ID|
+    deleteUrl = @m_ApiUrl + "api/v1/delete/#{_ID}"
+    @m_Response = HTTParty.delete(deleteUrl)
+end
+
+Then("API should return a {string} with message: {string} and status code: {int}") do |_Status, _Message, _Code|
+    expect(@m_Response.parsed_response["status"]).to eql(_Status)
+    expect(@m_Response.code).to eql(_Code)
+    expect(@m_Response.parsed_response["message"]).to eql(_Message)
 end
